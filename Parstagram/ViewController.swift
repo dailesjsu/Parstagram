d41528d4be631d030cd0c31cc0cd27e8ff1ed4de
Dai Le <dai.n.le@sjsu.edu>
2020-03-14 18:27:59 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Dai Le on 3/14/20.
+//  Copyright © 2020 daile14011997@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
