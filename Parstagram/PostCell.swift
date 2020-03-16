fc4b03758e1d9f6a0308aac706cb363877101998
Dai Le <dai.n.le@sjsu.edu>
2020-03-15 21:25:29 -0700

    Parstagram part 1

---
+++ Parstagram/PostCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostCell.swift
+//  Parstagram
+//
+//  Created by Dai Le on 3/15/20.
+//  Copyright © 2020 daile14011997@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
